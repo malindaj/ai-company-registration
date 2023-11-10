521717e964cdccc0734f241c10f6c2711d31ab98
SameeraIndika <sameeraindikaN@gmail.com>
2023-11-10 17:28:20 +0530

    feat: Sidebar routes updated.

--- app/(dashboard)/(routes)/billing/page.tsx
+++ app/(dashboard)/(routes)/billing/page.tsx
@@ -8,7 +8,7 @@ import { cn } from "@/lib/utils";
 
 import { tools } from "@/constants";
 
-export default function HomePage() {
+export default function Billing() {
   const router = useRouter();
 
   return (
@@ -18,12 +18,12 @@ export default function HomePage() {
           Explore the power of AI Companies
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Register your company today and get your ABN in minutes.
+          Register your business today and get your ABN in minutes.
         </p>
       </div>
       <div className="mb-4 px-4 md:px-20 lg:px-32 space-y-4">
         <h2 className="text-xl md:text-2xl font-medium text-left">
-          Register your company
+          What would you like to do?
         </h2>
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
